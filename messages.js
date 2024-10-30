929f28bae8191e5361aa98651f3c65d6748ba76e
Saniya Patil <saniyapatil@Saniyas-MacBook-Air.local>
2024-10-30 22:21:01 +0530

    added the scroll to top button,worked in messges.css and index.html

--- messages.js
+++ messages.js
@@ -11,3 +11,26 @@ if (messages) {
     messages.style.top = "43%";
   };
 }
+
+//scroll to top button
+
+window.onload = function() {
+  let mybutton = document.getElementById("scrollToTopBtn");
+
+  window.onscroll = function() {
+      scrollFunction();
+  };
+
+  function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          mybutton.style.display = "block";
+      } else {
+          mybutton.style.display = "none";
+      }
+  }
+
+  mybutton.onclick = function() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+  };
+};
